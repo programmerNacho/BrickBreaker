ee9c7bd243ac3597d08e83b65ec7431609339845
programmerNacho <ignacio.programmer@gmail.com>
2020-02-03 12:52:56 +0100

    Particles implemeted.

--- BrickBreaker/Assets/Scripts/Brick.cs
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -6,6 +6,8 @@ public class Brick : MonoBehaviour
 {
     [SerializeField]
     private AudioClip ballBrick;
+    [SerializeField]
+    private ParticleSystem brickParticlePrefab;
 
     protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,6 +15,9 @@ public class Brick : MonoBehaviour
         {
             SoundManager.Instance.PlaySoundEffect(ballBrick);
             Camera.main.GetComponentInParent<CameraShaker>().Shake();
+            ParticleSystem particles = Instantiate(brickParticlePrefab, transform.position + new Vector3(0f, 0f, -5f), brickParticlePrefab.transform.rotation);
+            particles.Play();
+            Destroy(particles.gameObject, particles.main.duration);
             GameManager.Instance.DestroyBrick(this);
         }
     }
